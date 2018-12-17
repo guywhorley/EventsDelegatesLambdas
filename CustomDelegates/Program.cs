0e11adac79962bc7cd350b51f407604d3ea6fd71
Guy Whorley <guy.whorley@cherwell.com>
2018-12-17 15:01:11 -0800

    Add project files.

---
+++ CustomDelegates/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDelegates
+{
+	class Program
+	{
+		// delegate definition
+		public delegate int DoTheWorkHandler(string str1, WorkType wt);
+
+		public static void Main(string[] args)
+		{
+			// Wiring up the delegate pipeline to event handler.
+			DoTheWorkHandler del = new DoTheWorkHandler(DoTheWork);
+			DoTheWorkHandler del1 = new DoTheWorkHandler(DoOtherWork);
+			DoTheWorkHandler del2 = new DoTheWorkHandler(DoYetMoreWork);
+			
+			// order matters on multicast delegate
+			del += del1 + del2;
+
+			// FYI: the last delegate return value is the one that is used. They are not 'accumulated'.
+			int finalHours = del("Chris", WorkType.Shopping);
+			Console.WriteLine($"finalHours: {finalHours}");
+			
+			
+			
+			
+			
+			Console.Read(); // Exit run
+		}
+
+		// Invoke the delegate
+		static void DoWork(DoTheWorkHandler del) => del("Chris", WorkType.Work);
+
+		// handlers --- the method that actually does the work.
+		static int DoTheWork(string greeting, WorkType wt)
+		{
+			Console.WriteLine($"1) Do work one way: {greeting}. Here is your worktype: {wt.ToString()}");
+			return 1;
+		}
+
+		static int DoOtherWork(string greeting, WorkType wt)
+		{
+			Console.WriteLine($"2) Do Other Work a different way: {greeting}");
+			return 2;
+		}
+		static int DoYetMoreWork(string str1, WorkType wt)
+		{
+			Console.WriteLine($"3) Doing the work a third way {str1}");
+			return 3;
+		}
+	}
+
+	public enum WorkType
+	{
+		NONE,
+		Shopping,
+		Work
+	}
+}
---
+++ WindowsFormExample/BigRedButton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormExample
+{
+	public partial class BigRedButton : Form
+	{
+		public BigRedButton()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Button was clicked");
+
+		}
+
+		private void button2_MouseUp(object sender, MouseEventArgs e)
+		{
+			MessageBox.Show(e.X.ToString());
+				
+		}
+	}
+}
