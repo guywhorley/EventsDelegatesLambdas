ddee70fe81dee97a0d7ff1ec5c373ca28694d98b
Guy Whorley <guy.whorley@cherwell.com>
2018-12-21 15:35:13 -0800

    First Commit - Working

---
+++ CommunicatingBetweenControls/CommunicatingBetweenControls/Mediator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunicatingBetweenControls.Model;
+
+namespace CommunicatingBetweenControls
+{
+	/// <summary>
+	/// Entity used to communiated between controls keepign very loose coupling.
+	/// Keeping sealed so it cannot be overridden.
+	/// </summary>
+	public sealed class Mediator
+	{
+		// static members
+		// BEGIN SINGLETON SETUP
+		private static readonly Mediator _instance = new Mediator();
+		
+		// Keep contructor private in the singleton pattern.
+		private Mediator() {}
+
+		/// <summary>
+		/// Get the Mediator singleton.
+		/// </summary>
+		/// <returns></returns>
+		public static Mediator GetInstance() => _instance;
+		// END SINGLETON SETUP
+	
+		// instance functionality
+		public event EventHandler<JobChangedEventArgs> JobChanged;
+
+		// What occurs when JobChanged
+		public void OnJobChanged(object sender, Job job)
+		{
+			// get the handler
+			// pattern: create event; create On<Event> method; in the method,
+			// define delegate = <event> as EventHandler...; Invoke the delegate
+			var jobChangedDelegate = JobChanged as EventHandler<JobChangedEventArgs>;
+			jobChangedDelegate?.Invoke(sender, new JobChangedEventArgs { Job = job });
+		}
+	}
+}
