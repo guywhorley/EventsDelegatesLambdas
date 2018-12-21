94da46cba2befd32223d8ba3988cb6505ac3dda3
Guy Whorley <guy.whorley@cherwell.com>
2018-12-20 22:17:02 -0800

    Added comments.

--- Events/Program.cs
+++ Events/Program.cs
@@ -36,12 +36,24 @@ namespace Events
 	public class Worker
 	{
 		// events and delegates
+		
+		// the event must reference a delegate
+		/// <summary>
+		/// Raise a notification to subscribers
+		/// </summary>
 		public event SubscribeToNotifications NotifySubscribers;
+		
+		// the delegate is used in the client classes 
+		/// <summary>
+		/// Subscribe to an event.
+		/// </summary>
+		/// <param name="s"></param>
 		public delegate void SubscribeToNotifications(string s);
 		
 		public int DoSomeWork(string str)
 		{
 			Console.WriteLine($"In DoSomeWork(): Raising OnWorkEventRaiser({str})");
+			// raise the event which in turn will signal all the subscribers
 			NotifySubscribers?.Invoke(str);
 			return 1;
 		}
